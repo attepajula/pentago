479367e66185332f3f0301577ced1e02d886ae37
Atte Pajula <attepajula@yahoo.com>
2024-05-03 19:32:36 +0300

    Add is_terminal check

--- src/pentago.py
+++ src/pentago.py
@@ -4,6 +4,7 @@ from random import randint
 
 def your_turn():
     # Your turn
+    # Need to raise an error in case of string in place of integer!!!!!
     row = int(input("Choose row (1-6): ")) - 1
     col = int(input("Choose column (1-6): ")) - 1
     quadrant = int(input("Choose quarter (1-4): ")) - 1
@@ -19,11 +20,11 @@ def play():
     board.print_board()
 
     # Main loop
-    while not board.is_terminal:
+    while not board.terminal:
         if current_player == 1:
             print(f"Current player {current_player}")
             print("Your turn:")
-
+            
             while True:
                 # Your turn
                 moves = your_turn()
@@ -40,12 +41,13 @@ def play():
             legal_moves = generate_legal_moves(board)
             if legal_moves:
                 for move in legal_moves:
-                    eva = minimax(move, depth=3, maximizingPlayer=False)
+                    eva = minimax(move, depth=50, maximizingPlayer=False)
                     if eva > best_value:
                         best_value = eva
                         best_move = move
                 board = best_move
-
+        if board.is_terminal():
+            board.terminal = True
         board.print_board()
 
         # Change turns
