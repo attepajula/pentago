4fa77c2b438ecd7a717537dad10017b7984170c0
Atte Pajula <attepajula@yahoo.com>
2024-05-03 18:42:34 +0300

    Add evaluate func

--- src/algo.py
+++ src/algo.py
@@ -2,7 +2,6 @@ from board import PentagoBoard
 from math import inf
 
 def minimax(board, depth, maximizingPlayer):
-    return 0
     if depth == 0 or board.is_terminal:
         return evaluate(board)
 
@@ -20,7 +19,27 @@ def minimax(board, depth, maximizingPlayer):
         return minEval
     
 def evaluate(board):
-    return 0
+    player_symbol = "O"
+    opponent_symbol = "X"
+    player_score = 0
+    opponent_score = 0
+
+    for i in range(6):
+        row = ''.join(board.state[i])
+        col = ''.join([board.state[j][i] for j in range(6)])
+        player_score += row.count(player_symbol) + col.count(player_symbol)
+        opponent_score += row.count(opponent_symbol) + col.count(opponent_symbol)
+
+    for i in range(-2, 4):
+        diag1 = ''.join([board.state[i + j][j] for j in range(max(0, -i), min(6, 6 - i))])
+        diag2 = ''.join([board.state[j][i + j] for j in range(max(0, -i), min(6, 6 - i))])
+        diag3 = ''.join([board.state[5 - j][i + j] for j in range(max(0, -i), min(6, 6 - i))])
+        diag4 = ''.join([board.state[5 - i - j][j] for j in range(max(0, -i), min(6, 6 - i))])
+        player_score += diag1.count(player_symbol) + diag2.count(player_symbol) + diag3.count(player_symbol) + diag4.count(player_symbol)
+        opponent_score += diag1.count(opponent_symbol) + diag2.count(opponent_symbol) + diag3.count(opponent_symbol) + diag4.count(opponent_symbol)
+
+    return player_score - opponent_score
+
 
 def generate_legal_moves(board):
     legal_moves = []
