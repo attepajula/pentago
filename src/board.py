a75d6526434e9394796c7451402fa22fbb605d8f
Atte Pajula <attepajula@yahoo.com>
2024-05-07 19:16:12 +0300

    Change board implementation

--- src/board.py
+++ src/board.py
@@ -1,30 +1,34 @@
 class PentagoBoard:
     def __init__(self):
-        self.state = [["_" for _ in range(6)] for _ in range(6)]
+        self.state = [[0 for _ in range(6)] for _ in range(6)]
         self.terminal = False
         self.legal_moves = []
 
     def print_board(self):
+        horizontal_line = "+---------+"
+        print(horizontal_line)
         for row in self.state:
-            print(' '.join(map(str, row)))
-        print()
+            row = " ".join(["X" if cell == 2 else "O" if cell == 1 else "_" for cell in row])
+            print(row)
+        print(horizontal_line)
+
 
     def make_move(self, row, col, quadrant, player, direction): 
         if not (0 <= row < 6 and 0 <= col < 6 and 0 <= quadrant <= 3):
             print("Invalid input!")
             return False
-        if direction.lower() not in ["cw", "ccw"]:
+        if direction not in [1, -1]:
             print("Invalid input!")
             return False
-        if self.state[row][col] == "_":
-            self.state[row][col] = "O" if player == 1 else "X"
+        if self.state[row][col] == 0:
+            self.state[row][col] = 1 if player == 1 else 2
         else:
             print("Position occupied!")
             return False
         if 0 <= quadrant < 4:
-            if direction == "CW":
+            if direction == 1:
                 self.rotate(quadrant, counterclockwise=False)
-            elif direction == "CCW":
+            elif direction == -1:
                 self.rotate(quadrant, counterclockwise=True)
         return True
 
@@ -52,23 +56,22 @@ class PentagoBoard:
     def is_terminal(self):
         for i in range(6):
             for j in range(2):
-                if self.state[i][j] != "_" and self.state[i][j] == self.state[i][j+1] == self.state[i][j+2] == self.state[i][j+3] == self.state[i][j+4]:
+                if self.state[i][j] != 0 and self.state[i][j] == self.state[i][j+1] == self.state[i][j+2] == self.state[i][j+3] == self.state[i][j+4]:
                     self.terminal = True
 
         for i in range(2):
             for j in range(6):
-                if self.state[i][j] != "_" and self.state[i][j] == self.state[i+1][j] == self.state[i+2][j] == self.state[i+3][j] == self.state[i+4][j]:
+                if self.state[i][j] != 0 and self.state[i][j] == self.state[i+1][j] == self.state[i+2][j] == self.state[i+3][j] == self.state[i+4][j]:
                     self.terminal = True
 
         for i in range(2):
             for j in range(2):
-                if self.state[i][j] != "_" and self.state[i][j] == self.state[i+1][j+1] == self.state[i+2][j+2] == self.state[i+3][j+3] == self.state[i+4][j+4]:
+                if self.state[i][j] != 0 and self.state[i][j] == self.state[i+1][j+1] == self.state[i+2][j+2] == self.state[i+3][j+3] == self.state[i+4][j+4]:
                     self.terminal = True
-                if self.state[i][5-j] != "_" and self.state[i][5-j] == self.state[i+1][4-j] == self.state[i+2][3-j] == self.state[i+3][2-j] == self.state[i+4][1-j]:
+                if self.state[i][5-j] != 0 and self.state[i][5-j] == self.state[i+1][4-j] == self.state[i+2][3-j] == self.state[i+3][2-j] == self.state[i+4][1-j]:
                     self.terminal = True
 
-        if "_" not in [symbol for row in self.state for symbol in row]:
+        if 0 not in [symbol for row in self.state for symbol in row]:
             self.terminal = True
 
         return self.terminal
-
