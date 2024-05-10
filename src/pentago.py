a9c090ccf9dde4a07d92ebabceff0b95bfa97416
Atte Pajula <attepajula@yahoo.com>
2024-05-10 16:28:27 +0300

    Add loading animation

--- src/pentago.py
+++ src/pentago.py
@@ -1,6 +1,49 @@
 from board import *
 from algo import *
 from random import randint
+from itertools import cycle
+from shutil import get_terminal_size
+from threading import Thread
+from time import sleep
+
+class Loader:
+    def __init__(self, desc="Loading...", end="Done!", timeout=0.1):
+        """
+        A loader-like context manager
+
+        Args:
+            desc (str, optional): The loader's description. Defaults to "Loading...".
+            end (str, optional): Final print. Defaults to "Done!".
+            timeout (float, optional): Sleep time between prints. Defaults to 0.1.
+        """
+        self.desc = desc
+        self.end = end
+        self.timeout = timeout
+
+        self._thread = Thread(target=self._animate, daemon=True)
+        self.steps = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
+        self.done = False
+
+    def start(self):
+        self._thread.start()
+        return self
+
+    def _animate(self):
+        for c in cycle(self.steps):
+            if self.done:
+                break
+            print(f"\r{self.desc} {c}", flush=True, end="")
+            sleep(self.timeout)
+
+    def __enter__(self):
+        self.start()
+
+    def stop(self):
+        self.done = True
+        cols = get_terminal_size((80, 20)).columns
+        print("\r" + " " * cols, end="", flush=True)
+        print(f"\r{self.end}", flush=True)
+
 
 def your_turn():
     # Your turn
@@ -28,7 +71,6 @@ def play():
     while board.terminal == 0:
         if current_player == 1:
             print("Your turn:")
-            
             while True:
                 # Your turn
                 try:
@@ -39,12 +81,12 @@ def play():
                     print("Invalid input!")
 
         if current_player == 2:
-            print(f"Current player {current_player}")
             print("Machine makes a move:")
+            loader = Loader("Making move...", "").start()
             best_move = minimax(board, depth=0, max_depth=2, maximizingPlayer=True)[0]
             if best_move:
                 board = best_move
-
+            loader.stop()
         board.is_terminal()
         board.print_board()
 
