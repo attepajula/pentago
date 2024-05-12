e8bebd866b43172f3a4a03899db9269447934568
Atte Pajula <attepajula@yahoo.com>
2024-05-13 00:52:54 +0300

    Add doc strings

--- src/pentago.py
+++ src/pentago.py
@@ -47,6 +47,14 @@ class Loader: # Source: https://stackoverflow.com/a/66558182
 
 # Core functions for the game:
 def your_turn():
+    """Prompts the user to input their move.
+
+    Asks the user to choose the row, column, quadrant, and direction for their move.
+    Validates the input, alternates and returns it as a list containing row, column, quadrant, and direction.
+    
+    Returns:
+        list: A list containing the chosen row (0-5), column (0-5), quadrant (0-3), and direction (1 for clockwise, -1 for counterclockwise).
+    """
     # Your turn
     row = int(input("Choose row (1-6): ")) - 1
     col = int(input("Choose column (1-6): ")) - 1
@@ -62,6 +70,12 @@ def your_turn():
     return [row, col, quadrant, direction]
 
 def play():
+    """Controls the gameplay loop for Pentago.
+
+    Initializes a new PentagoBoard and determines the starting player randomly. 
+    Manages player and AI moves until the game reaches a terminal state.
+    Displays the current board state after each move and shows the game result when it's over.
+    """
     board = PentagoBoard()
     current_player = randint(1, 2)
 
@@ -83,7 +97,6 @@ def play():
                 except(KeyboardInterrupt):
                     print(" Game over!")
                     sys.exit()
-
 
         if current_player == 2:
             print("Machine makes a move:")
